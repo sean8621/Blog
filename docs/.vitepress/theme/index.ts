2a4822630744171db3db05528a5c66bd7393937b
sean86 <13926136912@163.com>
2025-08-08 01:33:05 +0800

    ✨ 更新主题配置，增强应用组件和高亮功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,13 +4,13 @@ import DefaultTheme from "vitepress/theme";
 import "./style.css";
 import Preview from "./preview/index.vue";
 import "highlight.js/styles/base16/summerfruit-light.css"; // 主题
-import hljsVuePlugin from "@highlightjs/vue-plugin";
+import highlightjs from "@highlightjs/vue-plugin";
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app, router, siteData }) {
     // ...
     app.component("preview", Preview);
-    app.use(hljsVuePlugin);
+    app.use(highlightjs);
   },
 } satisfies Theme;
