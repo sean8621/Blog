95548a3d20d9d80b974b58b27c4865b25edf4f1e
sean8621 <sean8621@users.noreply.github.com>
2025-08-08 07:32:51 +0000

    deploy: 350f96d0b817630a32d1168963afd266c982741f

---
+++ assets/chunks/VirtualListItem.BSi_NWjU.js
@@ -0,0 +1,30 @@
+const e=`<template>
+  <li :key="item.brandId" :style="{height: \`\${item.height}px\`, 'line-height': \`\${item.height}px\`}" ref="node">
+    <div>
+      <div>{{item.name}}</div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      default() {
+        return {}
+      },
+      type: Object
+    },
+    index: Number
+  },
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    this.$emit('update-height', {height: this.$refs.node.getBoundingClientRect().height, index: this.index})
+  }
+}
+<\/script>
+`;export{e as default};
